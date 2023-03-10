f2ec27e5d58931da0247b5fb0a8cadaa3312472f
Amaury Laridon <amaurylaridon@protonmail.com>
2023-03-10 10:45:46 +0100

    init

---
+++ TSIM.py
@@ -0,0 +1,6 @@
+# Thermodynamic Sea Ice Model (TSIM)
+# Author : Amaury Laridon
+# Course : LPHYS2269 - Remote Sensing of Climate Change
+# Goal : Build a thermodynamic sea ice model that can be able to simulate a full seasonal cycle of sea ice growth and melt.
+#        More information on the GitHub Page of the project :
+# Date : 10/03/23

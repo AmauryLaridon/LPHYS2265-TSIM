e29eea2969a8f80191fd9d5ae9c0718b5f9d7b7a
Amaury Laridon <amaurylaridon@protonmail.com>
2023-03-16 10:35:30 +0100

    Pts 1.2.3 Final V.1

--- IGCT1.py
+++ IGCT1.py
@@ -24,9 +24,13 @@ T_bo = -1.8 + kelvin  # Bottom temperature [K]
 N_days = 30  # number of days of integration
 h_i0 = 0.1  # initial sea ice thickness [m]
 T_su = -10 + kelvin  # air temperature [K]
+# Binary condition in order to implement ocean heat flux in the model. Will be used in 1.2
+ocean_heat_flux = False
+Q_w = 5  # oceanic heat flux[W/m²]
 ######################################## 1 Ice growth with constant temperature ############################################
 ##################################################### Model ################################################################
-### 1.1 Stefan and numerical solution for ice growth ###
+
+### Functions of the model ###
 
 
 def fourier_cond_flux(h, T_bo, T_su):
@@ -36,15 +40,23 @@ def fourier_cond_flux(h, T_bo, T_su):
     return Q_c
 
 
-def E_loss_bottom(ice_thick, T_su, T_bott):
-    """ Compute the total energy loss in one day at the bottom of the sea ice layer
+def E_net_bottom(ice_thick, T_su, T_bott, ocean_heat=ocean_heat_flux, Q_w=Q_w):
+    """ Compute the total energy loss in one day at the bottom of the sea ice layer (upward positive)
         [J/m^2]
     """
-    E_loss_through_ice = fourier_cond_flux(ice_thick, T_bo, T_su) * sec_per_day
-    return E_loss_through_ice
+    if ocean_heat:
+        E_loss_through_ice = fourier_cond_flux(
+            ice_thick, T_bo, T_su) * sec_per_day
+        E_gain_ocean_flux = Q_w * sec_per_day
+        E_net_bot = E_loss_through_ice - E_gain_ocean_flux
+    else:
+        E_loss_through_ice = fourier_cond_flux(
+            ice_thick, T_bo, T_su) * sec_per_day
+        E_net_bot = E_loss_through_ice
+    return E_net_bot
 
 
-def ice_thick(integration_range=N_days, T_su=T_su, T_bo=T_bo, h_i0=h_i0, ocean_heat_flux=False):
+def ice_thick(integration_range=N_days, T_su=T_su, T_bo=T_bo, h_i0=h_i0, ocean_heat=ocean_heat_flux, Q_w=Q_w):
     """Computation of the evolution of the sea ice thickness using Stefan's law. 
     An option gives the possibility to add an Oceanic heat flux. 
     This function returns an array with the sea ice thickness
@@ -53,7 +65,7 @@ def ice_thick(integration_range=N_days, T_su=T_su, T_bo=T_bo, h_i0=h_i0, ocean_h
     print("                   ICGT SEA ICE THIKNESS MODEL")
     print("------------------------------------------------------------------")
     print(
-        "Evolution of the sea ice thickness using numerical Stefan's law.\nintegration range = {} days, T_su = {:.2f} °C, T_bo = {:.2f} °C,\nh_i0 = {:.2f} m, ocean_heat_flux = {}".format(N_days, T_su - kelvin, T_bo-kelvin, h_i0, ocean_heat_flux))
+        "Evolution of the sea ice thickness using numerical Stefan's law.\nintegration range = {} days, T_su = {:.2f} °C, T_bo = {:.2f} °C,\nh_i0 = {:.2f} m, ocean_heat_flux = {}, Q_w = {} W/m²".format(N_days, T_su - kelvin, T_bo-kelvin, h_i0, ocean_heat_flux, Q_w))
     print("------------------------------------------------------------------")
     # array colecting the values of the sea ice thickness for each day
     h_i = np.zeros(N_days)
@@ -64,14 +76,15 @@ def ice_thick(integration_range=N_days, T_su=T_su, T_bo=T_bo, h_i0=h_i0, ocean_h
         print("Sea ice thickness at begining of Day {} = {:.2f} m".format(
             day, h_i[day-1]))
         # Energy lost at the bottom during one day due to flux from water to ice.[J/m^2]
-        E_loss = E_loss_bottom(h_i[day-1], T_su, T_bo)
+        E_net_bot = E_net_bottom(
+            h_i[day-1], T_su, T_bo, ocean_heat_flux, Q_w)
         # Mass of water freezed at the bottom of the ice layer at the end of one day [kg/m^2]
-        Freezing_water_mass = E_loss/L_fus
+        Freezing_water_mass = E_net_bot/L_fus
         # We obtain [m] as needed
         delta_h = Freezing_water_mass / rho_i
         h_i[day] = h_i[day-1] + delta_h
         print(
-            "Energy lost at the bottom during Day {} = {:.2f} MJ/m²".format(day, E_loss/1e6))
+            "Energy balance at the bottom during Day {} = {:.2f} MJ/m²".format(day, E_net_bot/1e6))
         print("Variation of sea-ice thickness during Day {} = {:.2f} m".format(
             day, delta_h))
         print("Sea ice thickness at the end of Day {} = {:.2f} m".format(
@@ -81,6 +94,7 @@ def ice_thick(integration_range=N_days, T_su=T_su, T_bo=T_bo, h_i0=h_i0, ocean_h
 
 
 def stefan_law(integration_range=N_days, T_su=T_su, T_bo=T_bo, h_i0=h_i0):
+    """Analytical solution of the sea ice thickness evolution following Stefan's law."""
     time_range = range(0, integration_range)  # integration range in days
     # array colecting the values of the sea ice thickness for each day
     H_t = np.zeros(N_days)
@@ -92,27 +106,30 @@ def stefan_law(integration_range=N_days, T_su=T_su, T_bo=T_bo, h_i0=h_i0):
     return H_t, time_range
 
 
+## Instanciation ##
 h_i, time_range = ice_thick()  # instanciation of the numerical model
 h_i_law, time_range_law = stefan_law()
 
 ## Display ##
-# 1.1.1
+plt.rcParams['text.usetex'] = True
+
+save_dir = "/home/amaury/Bureau/LPHYS2265 - Sea ice ocean atmosphere interactions in polar regions/Projet/Figures/"
 figure = plt.figure(figsize=(16, 10))
+# 1.1.1
 plt.plot(time_range, h_i, label="Numerical model")
 plt.title("ICGT Ice thickness evolution for {} days".format(N_days), size=26)
 plt.xlabel("Days", size=20)
 plt.ylabel("Ice thickness [m]", size=20)
 plt.legend(fontsize=18)
 plt.grid()
-save_dir = "/home/amaury/Bureau/LPHYS2265 - Sea ice ocean atmosphere interactions in polar regions/Projet/Figures/"
 plt.savefig(
     save_dir + "1.1.1.png", dpi=300)
 # plt.show()
+plt.clf()
 # 1.1.2
 print(
     "Thickness of the ice at the end of the simulation : {:.4f} m".format(h_i[-1]))
 # 1.1.3
-figure = plt.figure(figsize=(16, 10))
 plt.plot(time_range, h_i, label="Numerical model")
 plt.plot(time_range, h_i_law, label="Stefan's law")
 plt.title("ICGT Ice thickness evolution for {} days".format(N_days), size=26)
@@ -120,41 +137,61 @@ plt.xlabel("Days", size=20)
 plt.ylabel("Ice thickness [m]", size=20)
 plt.legend(fontsize=18)
 plt.grid()
-save_dir = "/home/amaury/Bureau/LPHYS2265 - Sea ice ocean atmosphere interactions in polar regions/Projet/Figures/"
 plt.savefig(
     save_dir + "1.1.3.png", dpi=300)
 # plt.show()
-print("Thickness of the ice at the end the period using Stefan's law : {:.4f} m ".format(
+plt.clf()
+print("Thickness of the ice at the end of the period using Stefan's law : {:.4f} m ".format(
     h_i_law[-1]))
 
 
-"""
 ### 1.2 Addition of an ocean heat flux ###
-F_w = 5  # oceanic heat flux[W/m²]
 
-evo_h_i_F_w = ice_thick(h_i, ocean_heat_flux=True,
-                        F_w=5)  # instanciation with F_w
-evo_h_i_F_w180 = ice_thick(h_i, ocean_heat_flux=True,
-                           F_w=180.4)  # instanciation with F_w
+## Instanciation ##
+# Binary condition in order to implement ocean heat flux in the model
+ocean_heat_flux = True
+h_i_F_w, time_range = ice_thick(
+    ocean_heat=ocean_heat_flux)  # instanciation with F_w
+
+
+## Display ##
+
 # 1.2.1
-plt.plot(np.arange(0, 30, 1), evo_h_i, label="Numerical model without F_w")
-plt.plot(np.arange(0, 30, 1), evo_h_i_F_w, label="Numerical model with F_w")
-plt.plot(np.arange(0, 30, 1), evo_h_i_law, label="Stefan's law")
-plt.title("Ice thickness evolution for 30 days", size=26)
+plt.plot(time_range, h_i_F_w, label="Numerical model with OHF")
+plt.plot(time_range, h_i, label="Numerical model without OHF")
+plt.title('ICGT Ice thickness evolution for {} days\nwith oceanic heat flux Q_w = {:.2f}W/m²'.format(
+    N_days, Q_w), size=26)
 plt.xlabel("Days", size=20)
 plt.ylabel("Ice thickness [m]", size=20)
-plt.legend()
+plt.legend(fontsize=18)
 plt.grid()
-plt.show()
-# 1.2.2
-print("Thickness of the ice at the end of the simulation : ",
-      evo_h_i_F_w[-1], "m.")
-# 1.2.3
-print("Thickness of the ice at the end of the simulation with F_w = 180.4W/m²: ",
-      evo_h_i_F_w180[-1], "m.")
-# 1.2.4
-h_1 = np.zeros(N_days)
-h_1[0] = 1
+plt.savefig(
+    save_dir + "1.2.1.png", dpi=300)
+# plt.show()
+plt.clf()
 
-### 1.3 Addition of snow on top of the ice ###
-"""
+# 1.2.2
+
+print(
+    "Thickness of the ice at the end of the simulation with OHF : {:.4f} m".format(h_i_F_w[-1]))
+
+# 1.2.3
+
+## Instanciation ##
+Q_w = 180.4
+h_i_F_w180, time_range = ice_thick(
+    ocean_heat=ocean_heat_flux, Q_w=180.4)  # instanciation with F_w
+
+## Display ##
+plt.plot(time_range, h_i_F_w180, label="Numerical model with OHF")
+# plt.plot(time_range, h_i, label="Numerical model without OHF")
+plt.title('ICGT Ice thickness evolution for {} days\nwith oceanic heat flux Q_w = {:.2f}W/m²'.format(
+    N_days, Q_w), size=26)
+plt.xlabel("Days", size=20)
+plt.ylabel("Ice thickness [m]", size=20)
+plt.legend(fontsize=18)
+plt.grid()
+plt.savefig(
+    save_dir + "1.2.3.png", dpi=300)
+# plt.show()
+plt.clf()

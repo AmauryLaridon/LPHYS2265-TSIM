3f4f37b0022438291ff0746f851428f085a52e3c
Amaury Laridon <amaurylaridon@protonmail.com>
2023-03-16 09:57:07 +0100

    Pts 1.1 Final V.1

--- IGCT1.py
+++ IGCT1.py
@@ -13,79 +13,122 @@ import matplotlib.pyplot as plt
 ################################################### Parameters #############################################################
 ### Physical Constant ###
 L_fus = 3.35e5  # Latent heat of fusion for water [J/kg]
-rhoi = 917  # Sea ice density [kg/m³]
+rho_i = 917  # Sea ice density [kg/m³]
 ki = 2.2  # Sea ice thermal conductivity [W/m/K]
 ks = 0.31  # Snow thermal conductivity [W/m/K]
 sec_per_day = 86400  # Seconds in one day [s/day]
 ### Bottom boundary conditions ###
-T_bo = -1.8 + 273.15  # Bottom temperature [K]
-
+kelvin = 273.15  # Conversion degre K -> C
+T_bo = -1.8 + kelvin  # Bottom temperature [K]
+### Parameters ###
+N_days = 30  # number of days of integration
+h_i0 = 0.1  # initial sea ice thickness [m]
+T_su = -10 + kelvin  # air temperature [K]
 ######################################## 1 Ice growth with constant temperature ############################################
 ##################################################### Model ################################################################
 ### 1.1 Stefan and numerical solution for ice growth ###
-# arry with the values of the sea ice thickness for the 30 days when we want to compute
-N_days = 30
-h_i = np.zeros(N_days)
-h_i[0] = 0.1  # initial condition for the sea ice thickness for the first day [m]
-T_air = -10 + 273.15  # air temperature [K]
 
 
-def fourier_cond_flux(h, T_f, T_su):
-    """Computation of the conductive heat flux trough the ice using the Fourier-Fick's law"""
-    F_c = ((T_f - T_su)/(h))*ki
-    return F_c
+def fourier_cond_flux(h, T_bo, T_su):
+    """Computation of the conductive heat flux Q_c trough the ice using the Fourier-Fick's law (upward positive)
+    [W/m^2]"""
+    Q_c = ((T_bo - T_su)/(h))*ki
+    return Q_c
 
 
-def ice_thick(h_i, ocean_heat_flux=False, F_w=0):
-    """Computation of the evolution of the sea ice thickness using Stefan's law. An option gives the possibility to add an Oceanic heat flux"""
-    print("Evolution of the sea ice thickness")
-    print("-----------------------------")
-    for t in range(1, N_days):
-        print("Day = {}/{}".format(t, N_days))
-        print("h_i[t-1] = ", h_i[t-1])
-        if ocean_heat_flux:
-            Q_c = fourier_cond_flux(h_i[t-1], T_bo, T_air)
-        else:
-            Q_c = fourier_cond_flux(h_i[t-1], T_bo, T_air) + F_w
-        print("Q_c = ", Q_c)
-        delta_h = Q_c * (1/rhoi*L_fus)
-        print("delta_h = ", delta_h)
-        h_new = h_i[t-1] + delta_h
-        print("h_new = ", h_new)
-        h_i[t] = h_new
-        print("-----------------------------")
-    return h_i
+def E_loss_bottom(ice_thick, T_su, T_bott):
+    """ Compute the total energy loss in one day at the bottom of the sea ice layer
+        [J/m^2]
+    """
+    E_loss_through_ice = fourier_cond_flux(ice_thick, T_bo, T_su) * sec_per_day
+    return E_loss_through_ice
 
 
-def stefan_law(t_0, T, h_0, k, deltaT, rhoi, L):
-    time = np.arange(t_0, T, 1)
+def ice_thick(integration_range=N_days, T_su=T_su, T_bo=T_bo, h_i0=h_i0, ocean_heat_flux=False):
+    """Computation of the evolution of the sea ice thickness using Stefan's law. 
+    An option gives the possibility to add an Oceanic heat flux. 
+    This function returns an array with the sea ice thickness
+     and an array with the time of integration"""
+    print("------------------------------------------------------------------")
+    print("                   ICGT SEA ICE THIKNESS MODEL")
+    print("------------------------------------------------------------------")
+    print(
+        "Evolution of the sea ice thickness using numerical Stefan's law.\nintegration range = {} days, T_su = {:.2f} °C, T_bo = {:.2f} °C,\nh_i0 = {:.2f} m, ocean_heat_flux = {}".format(N_days, T_su - kelvin, T_bo-kelvin, h_i0, ocean_heat_flux))
+    print("------------------------------------------------------------------")
+    # array colecting the values of the sea ice thickness for each day
+    h_i = np.zeros(N_days)
+    h_i[0] = h_i0  # initial condition for the sea ice thickness for the first day [m]
+    time_range = range(0, integration_range)  # integration range in days
+    for day in range(1, integration_range):
+        print("Day {}/{}".format(day, integration_range))
+        print("Sea ice thickness at begining of Day {} = {:.2f} m".format(
+            day, h_i[day-1]))
+        # Energy lost at the bottom during one day due to flux from water to ice.[J/m^2]
+        E_loss = E_loss_bottom(h_i[day-1], T_su, T_bo)
+        # Mass of water freezed at the bottom of the ice layer at the end of one day [kg/m^2]
+        Freezing_water_mass = E_loss/L_fus
+        # We obtain [m] as needed
+        delta_h = Freezing_water_mass / rho_i
+        h_i[day] = h_i[day-1] + delta_h
+        print(
+            "Energy lost at the bottom during Day {} = {:.2f} MJ/m²".format(day, E_loss/1e6))
+        print("Variation of sea-ice thickness during Day {} = {:.2f} m".format(
+            day, delta_h))
+        print("Sea ice thickness at the end of Day {} = {:.2f} m".format(
+            day, h_i[day]))
+        print("------------------------------------------------------------------")
+    return h_i, time_range
+
+
+def stefan_law(integration_range=N_days, T_su=T_su, T_bo=T_bo, h_i0=h_i0):
+    time_range = range(0, integration_range)  # integration range in days
+    # array colecting the values of the sea ice thickness for each day
     H_t = np.zeros(N_days)
-    for t in time:
-        h_t = np.sqrt(h_0**2 + (2*k*deltaT*t)/(rhoi*L))
-        H_t[t] = h_t
-    return H_t
+    deltaT = T_bo - T_su
+    for day in range(1, integration_range):
+        day_sec = day * sec_per_day
+        h_day = np.sqrt(h_i0**2 + (2*ki*deltaT*day_sec)/(rho_i*L_fus))
+        H_t[day] = h_day
+    return H_t, time_range
 
 
-evo_h_i = ice_thick(h_i)  # instanciation modèle numérique
-evo_h_i_law = stefan_law(1, 30, h_i[0], ki, T_bo - T_air, rhoi, L_fus)
+h_i, time_range = ice_thick()  # instanciation of the numerical model
+h_i_law, time_range_law = stefan_law()
 
-## Affichage ##
+## Display ##
 # 1.1.1
-plt.plot(np.arange(0, 30, 1), evo_h_i, label="Numerical model")
-plt.plot(np.arange(0, 30, 1), evo_h_i_law, label="Stefan's law")
-plt.title("Ice thickness evolution for 30 days", size=26)
+figure = plt.figure(figsize=(16, 10))
+plt.plot(time_range, h_i, label="Numerical model")
+plt.title("ICGT Ice thickness evolution for {} days".format(N_days), size=26)
 plt.xlabel("Days", size=20)
 plt.ylabel("Ice thickness [m]", size=20)
-plt.legend()
+plt.legend(fontsize=18)
 plt.grid()
-plt.show()
+save_dir = "/home/amaury/Bureau/LPHYS2265 - Sea ice ocean atmosphere interactions in polar regions/Projet/Figures/"
+plt.savefig(
+    save_dir + "1.1.1.png", dpi=300)
+# plt.show()
 # 1.1.2
-print("Thickness of the ice at the end of the simulation : ",
-      evo_h_i[-1], "m.")
+print(
+    "Thickness of the ice at the end of the simulation : {:.4f} m".format(h_i[-1]))
 # 1.1.3
-print("Thickness of the ice at the end the period using Stefan's law : ",
-      evo_h_i_law[-1], "m.")
+figure = plt.figure(figsize=(16, 10))
+plt.plot(time_range, h_i, label="Numerical model")
+plt.plot(time_range, h_i_law, label="Stefan's law")
+plt.title("ICGT Ice thickness evolution for {} days".format(N_days), size=26)
+plt.xlabel("Days", size=20)
+plt.ylabel("Ice thickness [m]", size=20)
+plt.legend(fontsize=18)
+plt.grid()
+save_dir = "/home/amaury/Bureau/LPHYS2265 - Sea ice ocean atmosphere interactions in polar regions/Projet/Figures/"
+plt.savefig(
+    save_dir + "1.1.3.png", dpi=300)
+# plt.show()
+print("Thickness of the ice at the end the period using Stefan's law : {:.4f} m ".format(
+    h_i_law[-1]))
 
+
+"""
 ### 1.2 Addition of an ocean heat flux ###
 F_w = 5  # oceanic heat flux[W/m²]
 
@@ -114,3 +157,4 @@ h_1 = np.zeros(N_days)
 h_1[0] = 1
 
 ### 1.3 Addition of snow on top of the ice ###
+"""
